846b88c6fbfded01e09fe04b1e005d5aef54fdb6
Mike Zimin <mzimin@dreamheads.ru>
2023-01-18 22:05:50 +0600

    first commit

---
+++ enum/1-index.js
@@ -0,0 +1,9 @@
+var Statuses;
+(function (Statuses) {
+    Statuses[Statuses["New"] = 0] = "New";
+    Statuses[Statuses["OnModeration"] = 1] = "OnModeration";
+    Statuses[Statuses["Public"] = 2] = "Public";
+    Statuses[Statuses["Canceled"] = 3] = "Canceled";
+})(Statuses || (Statuses = {}));
+;
+console.log(Statuses.New);
---
+++ enum/1-index.ts
@@ -0,0 +1,8 @@
+enum Statuses {
+    New,
+    OnModeration,
+    Public,
+    Canceled
+};
+
+console.log(Statuses.New);
